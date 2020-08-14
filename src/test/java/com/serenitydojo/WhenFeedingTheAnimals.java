76475cfc7a4d608dfd251256ef9219dcc68d1ee4
sivareddy1 <siva.reddy2211@gmail.com>
2020-08-14 18:47:44 +0100

    Until Switches & Enums Session part 2

--- src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
+++ src/test/java/com/serenitydojo/WhenFeedingTheAnimals.java
@@ -6,6 +6,11 @@ import com.serenitydojo.model.FoodType;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static com.serenitydojo.model.AnimalType.*;
 
 
@@ -69,6 +74,67 @@ public class WhenFeedingTheAnimals {
         FoodType food = feeder.feeds(DOG,true);
         System.out.println(food);
         Assert.assertEquals(FoodType.PREMIUM_DOG_FOOD,food);
+    }
+
+    @Test
+    public void collections(){
+
+        // Arrays
+
+        String[] colors = {"Red","Blue","Green"};
+        int[] ages = {1,1,1};
+        String[] arrayOfColors = new String[4];
+        int[] arrayOfNumbers = new int[5];
+        arrayOfColors[0]="Red";
+        arrayOfColors[1]="Blue";
+        arrayOfColors[2]="Green";
+        arrayOfColors[3]="Yellow";
+        //System.out.println("First Color is " + arrayOfColors[0]);
+
+        //Loops
+
+        for (int i=0;i<4;i++){
+            System.out.println("Color is " + arrayOfColors[i] );
+            }
+
+        System.out.println("-----------------------------------------------------");
+        //Lists
+
+        List<Integer> myNumbers = new ArrayList<>();
+        List<String> myColors = new ArrayList<>();
+
+        myColors.add("Red"); // .add is method in List class which allows us to add variable value
+        myColors.add("Blue");
+        myColors.add("Green");
+        myColors.add("Yellow");
+        myColors.add("Purple");
+        myColors.add("Black");
+        myColors.add("White");
+        myColors.add("Red");
+
+        System.out.println(myColors.get(0)); // .get is method in List class which allows us to add variable value
+        System.out.println(myColors.get(6));
+        System.out.println("-----------------------------------------------------");
+        for(String someColor:myColors){
+            System.out.println("Color is : " + someColor);
+        }
+        System.out.println("-----------------------------------------------------");
+
+        Set<Integer> setOfMyNumbers = new HashSet<>();
+        Set<String> setOfMyColors = new HashSet<>();
+
+        setOfMyColors.add("Red"); // .add is method in Set class which allows us to add variable value
+        setOfMyColors.add("Blue");
+        setOfMyColors.add("Green");
+        setOfMyColors.add("Yellow");
+        setOfMyColors.add("Purple");
+        setOfMyColors.add("Black");
+        setOfMyColors.add("White");
+        setOfMyColors.add("Red");
+
+        for(String someColor:setOfMyColors){
+            System.out.println("Color is : " + someColor);
+        }
 
     }
 
